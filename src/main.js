117ed5325991887528201e295258ee95a9e09ec3
Colin M. Ford <ford.colin@gmail.com>
2017-01-05 22:55:38 -0500

    Setting up bootstrap and configuring webpack to package it correctly

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// require("normalize.css")
+// require("flexboxgrid")
+require("bootstrap/scss/bootstrap-flex.scss")
+require("./main.scss")
+
+console.log("hello")
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: "./src/main.js",
+    output: {
+        path: __dirname + "/docs",
+        filename: "main.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                query: {
+                    presets: ["es2015"]
+                }
+            }, {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+            }, {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            }, {
+                test: /\.png$/,
+                loader: "url-loader?limit=100000"
+            }, {
+                test: /\.jpg$/,
+                loader: "file-loader"
+            }, {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+            }, {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+            }, {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file"
+            }, {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=image/svg+xml"
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('[name].css'),
+        new HtmlWebpackPlugin({template: "./src/index.html"})
+    ]
+};
