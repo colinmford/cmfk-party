3ecfc007fe15ae77ccd7919430a6aeec6597d436
Colin M. Ford <ford.colin@gmail.com>
2017-02-12 17:36:36 -0500

    moving to production

--- src/main.js
+++ src/main.js
@@ -1,9 +1,2 @@
-// require("normalize.css")
-// require("flexboxgrid")
-require("jquery")
-require("tether")
-require("bootstrap/dist/js/bootstrap")
 require("bootstrap/scss/bootstrap.scss")
 require("./main.scss")
-
-// console.log("hello")
